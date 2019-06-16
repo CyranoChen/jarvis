dec4f95ec9169abe6c62f1a5823f2b5d94b07179
CyranoChen <cyrano@arsenalcn.com>
2019-06-16 10:34:38 +0800

    Almost final version

--- public/resource/js/main.js
+++ public/resource/js/main.js
@@ -1,6 +1,6 @@
 $(function () {
-    $.getScript('https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/js/bootstrap.min.js');
-    $.getScript('https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.10/js/adminlte.min.js');
+    $.getScript('/resource/js/bootstrap.min.js');
+    $.getScript('/resource/js/adminlte.min.js');
 
     // load shared web parts
     $("#header").load("./shared/header.html");
